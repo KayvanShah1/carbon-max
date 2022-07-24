e069b6d0c913f3c12f69f7c6eb4a2ed488def887
Kayvan Shah <kayvan.shah@climateconnect.digital>
2022-07-24 12:57:18 +0530

    added google-beta provider

---
+++ deploy/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.29.0"
+    }
+    google-beta = {
+      version = "~> 3.83.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = var.project_id
+  credentials = var.service_account_key_json_path
+}
+
+provider "google-beta" {
+  project     = var.project_id
+  credentials = var.service_account_key_json_path
+}
