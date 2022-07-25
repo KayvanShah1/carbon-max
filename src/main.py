c208b190e017edb2d5f7fbd48c812555e9e3872f
Kayvan Shah <kayvan.shah@climateconnect.digital>
2022-07-25 12:04:42 +0530

    path change

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import base64
-from lib2to3.pgen2.pgen import generate_grammar
 import functions_framework
 from datetime import datetime
 
@@ -7,17 +6,25 @@ from bucket import push_json_to_buffer_bucket, connect_to_buffer_bucket
 
 
 def generate_path(date):
-    path = f"sample_data/{date.year}/{date.month}/json_{date.day}.json"
+    path = (
+        f"sample_data/year={date.year}/month={date.month}/day={date.day}"
+        f"/file_{date.strftime('%Y%m%d%H%M%S')}.json"
+    )
     return path
 
 
 @functions_framework.cloud_event
 # Triggered from a message on a Cloud Pub/Sub topic.
 def test_function(cloud_event):
+    # Connect to the buffer bucket
+    # Create a path where the message is to be pushed
     bucket = connect_to_buffer_bucket("optical-unison-356814-test-bucket")
     path = generate_path(datetime.now())
 
-    # Print out the data from Pub/Sub, to prove that it worked
+    # Pull the message from the subscription
+    # Decode the binary message to make to JSON compatible
     message = cloud_event.data["message"]
     message["data"] = base64.b64decode(message["data"]).decode("utf-8")
+
+    # Save the JSON message in GCS Bukcet
     push_json_to_buffer_bucket(bucket, message, path)
