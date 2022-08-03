71dd37823c2dc73d3c292e18cc59912f1df58cfb
Kayvan Shah <kayvan.shah@climateconnect.digital>
2022-08-04 01:09:10 +0530

    zipfile path change

--- deploy/data.tf
+++ deploy/data.tf
@@ -1,11 +1,11 @@
 data "archive_file" "source" {
   type        = "zip"
   source_dir  = var.source_code
-  output_path = "/tmp/function.zip"
+  output_path = "${path.root}/tmp/function.zip"
 }
 
 data "archive_file" "lambda_function_source" {
   type        = "zip"
   source_dir  = var.lambda_func_source_code
-  output_path = "/tmp/lambda_function.zip"
+  output_path = "${path.root}/tmp/lambda_function.zip"
 }
